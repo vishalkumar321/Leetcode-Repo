1e1d76f61de784767285428497acf8078e5c637b
Vishal kumar <vishalkumargupta4546@gmail.com>
2025-09-21 17:54:28 +0530

    Time: 403 ms (45.06%) | Memory: 211.6 MB (37.00%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,36 +1,67 @@
+// BOTTOM-UP APPROACH
 class Solution {
 public:
-    int solve(int index, int buy, int cap, vector<int>& prices,
-              vector<vector<vector<int>>>& dp) {
-        int n = prices.size();
-
-        if (cap == 0)
-            return 0;
-        if (index == n)
-            return 0;
-        if (dp[index][buy][cap] != -1) {
-            return dp[index][buy][cap];
-        }
-
-        int profit = 0;
-        if (buy) {
-            profit = max(-prices[index] + solve(index + 1, 0, cap, prices, dp),
-                         solve(index + 1, 1, cap, prices, dp));
-        } else {
-            profit =
-                max(prices[index] + solve(index + 1, 1, cap - 1, prices, dp),
-                    solve(index + 1, 0, cap, prices, dp));
-        }
-
-        dp[index][buy][cap] = profit;
-        return dp[index][buy][cap];
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-
         vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return solve(0, 1, 2, prices, dp);
+            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    int profit = 0;
+
+                    if (buy) {
+                        profit = max(-prices[index] + dp[index + 1][0][cap],
+                                     0 + dp[index + 1][1][cap]);
+                    } else {
+                        profit = max(prices[index] + dp[index + 1][1][cap - 1],
+                                     0 + dp[index + 1][0][cap]);
+                    }
+                    dp[index][buy][cap] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 };
+
+// TOP-DOWN APPROACH
+// class Solution {
+// public:
+//     int solve(int index, int buy, int cap, vector<int>& prices,
+//               vector<vector<vector<int>>>& dp) {
+//         int n = prices.size();
+
+//         if (cap == 0)
+//             return 0;
+//         if (index == n)
+//             return 0;
+//         if (dp[index][buy][cap] != -1) {
+//             return dp[index][buy][cap];
+//         }
+
+//         int profit = 0;
+//         if (buy) {
+//             profit = max(-prices[index] + solve(index + 1, 0, cap, prices,
+//             dp),
+//                          solve(index + 1, 1, cap, prices, dp));
+//         } else {
+//             profit =
+//                 max(prices[index] + solve(index + 1, 1, cap - 1, prices, dp),
+//                     solve(index + 1, 0, cap, prices, dp));
+//         }
+
+//         dp[index][buy][cap] = profit;
+//         return dp[index][buy][cap];
+//     }
+
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+
+//         vector<vector<vector<int>>> dp(
+//             n, vector<vector<int>>(2, vector<int>(3, -1)));
+//         return solve(0, 1, 2, prices, dp);
+//     }
+// };
