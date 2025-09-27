8a8b6e554f3152c5aefbeb8334e2a88042078cca
Vishal kumar <vishalkumargupta4546@gmail.com>
2025-09-27 15:15:08 +0530

    Time: 25 ms (61.54%) | Memory: 13.8 MB (78.18%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,31 +1,62 @@
+// BOTTOM-UP APPROACH
 class Solution {
 public:
-    int solve(int i, int j, vector<int>& cuts, vector<vector<int>>& dp) {
-        if (i > j)
-            return 0;
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int mini = INT_MAX;
-        for (int index = i; index <= j; index++) {
-            int cost = cuts[j + 1] - cuts[i - 1] +
-                       solve(i, index - 1, cuts, dp) +
-                       solve(index + 1, j, cuts, dp);
-            mini = min(mini, cost);
-        }
-
-        dp[i][j] = mini;
-        return dp[i][j];
-    }
-
     int minCost(int n, vector<int>& cuts) {
+        
         cuts.push_back(n);
         cuts.insert(cuts.begin(), 0);
-        int m = cuts.size();
         sort(cuts.begin(), cuts.end());
+        
+        int m = cuts.size();
+        vector<vector<int>> dp(m, vector<int>(m, 0));
+        
+        for (int len = 1; len <= m-2; len++) { 
+            for (int i = 1; i + len - 1 <= m-2; i++) {
+                int j = i + len - 1;
+                dp[i][j] = INT_MAX;
+                for (int k = i; k <= j; k++) {
+                    int cost = cuts[j+1] - cuts[i-1] +
+                               (k > i ? dp[i][k-1] : 0) +
+                               (k < j ? dp[k+1][j] : 0);
+                    dp[i][j] = min(dp[i][j], cost);
+                }
+            }
+        }
 
-        vector<vector<int>> dp(m, vector<int>(m, -1));
-        return solve(1, m - 2, cuts, dp);
+        return dp[1][m-2];
     }
 };
+
+
+// TOP-DOWN APPROACH
+// class Solution {
+// public:
+//     int solve(int i, int j, vector<int>& cuts, vector<vector<int>>& dp) {
+//         if (i > j)
+//             return 0;
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+
+//         int mini = INT_MAX;
+//         for (int index = i; index <= j; index++) {
+//             int cost = cuts[j + 1] - cuts[i - 1] +
+//                        solve(i, index - 1, cuts, dp) +
+//                        solve(index + 1, j, cuts, dp);
+//             mini = min(mini, cost);
+//         }
+
+//         dp[i][j] = mini;
+//         return dp[i][j];
+//     }
+
+//     int minCost(int n, vector<int>& cuts) {
+//         cuts.push_back(n);
+//         cuts.insert(cuts.begin(), 0);
+//         int m = cuts.size();
+//         sort(cuts.begin(), cuts.end());
+
+//         vector<vector<int>> dp(m, vector<int>(m, -1));
+//         return solve(1, m - 2, cuts, dp);
+//     }
+// };
