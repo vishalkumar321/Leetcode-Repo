a896a3cad31768f5517f2f8a533d57d8b0b8e87e
Vishal kumar <vishalkumargupta4546@gmail.com>
2025-09-27 20:29:54 +0530

    Time: 203 ms (35.64%) | Memory: 13.8 MB (81.32%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.cpp
@@ -0,0 +1,32 @@
+// TOP-DOWN APPROACH
+class Solution {
+public:
+    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i > j) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int maxi = INT_MIN;
+        for (int index = i; index <= j; index++) {
+            int burst = nums[i - 1] * nums[index] * nums[j + 1] +
+                        solve(i, index - 1, nums, dp) +
+                        solve(index + 1, j, nums, dp);
+            maxi = max(maxi, burst);
+        }
+
+        dp[i][j] = maxi;
+        return dp[i][j];
+    }
+
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
+
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return solve(1, n, nums, dp);
+    }
+};
