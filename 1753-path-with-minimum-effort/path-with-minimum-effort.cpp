4da84c0b4dcdd8187e0eb31e2fbc6b8fa0e95a80
Vishal kumar <vishalkumargupta4546@gmail.com>
2025-10-28 22:59:33 +0530

    Time: 48 ms (72.88%) | Memory: 24.4 MB (77.66%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
+
+        int n = heights.size();
+        int m = heights[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+
+        dist[0][0] = 0;
+        pq.push({0, {0, 0}});
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        while (!pq.empty()) {
+            auto it = pq.top();
+            pq.pop();
+            int diff = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+
+            if (row == n - 1 && col == m - 1)
+                return diff;
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) {
+                    int maxEffort =
+                        max(abs(heights[row][col] - heights[nrow][ncol]), diff);
+
+                    if (maxEffort < dist[nrow][ncol]) {
+                        dist[nrow][ncol] = maxEffort;
+                        pq.push({maxEffort, {nrow, ncol}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
