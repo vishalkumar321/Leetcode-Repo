680540864c0657b24d06c8779c99406a96984851
Vishal kumar <vishalkumargupta4546@gmail.com>
2026-02-01 14:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (88.09%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    void dfs(int room, vector<vector<int>>& rooms, vector<bool>& visited) {
-        visited[room] = true;
-
-        for (int key : rooms[room]) {
-            if (!visited[key]) {
-                dfs(key, rooms, visited);
-            }
-        }
-    }
-
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
-        vector<bool> visited(n, false);
 
-        dfs(0, rooms, visited);
+        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(0);
+        visited[0] = true;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int key : rooms[node]) {
+                if (!visited[key]) {
+                    visited[key] = true;
+                    q.push(key);
+                }
+            }
+        }
 
         for (bool v : visited) {
             if (!v)
