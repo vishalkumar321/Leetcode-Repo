f791ef3391b6850060983153af046c10c1645562
Vishal kumar <vishalkumargupta4546@gmail.com>
2026-02-18 22:57:43 +0530

    Time: 29 ms (42.76%) | Memory: 18.5 MB (38.36%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+
+        int ans = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '0' || (grid[i][j] == '1' && visited[i][j])) {
+                    continue;
+                }
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    q.push({i, j});
+                    visited[i][j] = true;
+                }
+
+                while (!q.empty()) {
+                    int row = q.front().first;
+                    int col = q.front().second;
+                    q.pop();
+
+                    int delRow[] = {1, 0, -1, 0};
+                    int delCol[] = {0, 1, 0, -1};
+
+                    for (int i = 0; i < 4; i++) {
+                        int nrow = row + delRow[i];
+                        int ncol = col + delCol[i];
+
+                        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                            !visited[nrow][ncol] && grid[nrow][ncol] == '1') {
+                            visited[nrow][ncol] = true;
+                            q.push({nrow, ncol});
+                        }
+                    }
+                }
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
