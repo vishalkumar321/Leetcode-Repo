7d234f73c23e24e0e6dedc7356bf350ae459dd06
Vishal kumar <vishalkumargupta4546@gmail.com>
2026-02-18 23:07:22 +0530

    Time: 27 ms (56.75%) | Memory: 18.3 MB (39.24%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -6,38 +6,35 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         queue<pair<int, int>> q;
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '0' || (grid[i][j] == '1' && visited[i][j])) {
-                    continue;
-                }
-                if (grid[i][j] == '1' && !visited[i][j]) {
+
+                if (grid[i][j] == '1') {
                     q.push({i, j});
-                    visited[i][j] = true;
-                }
+                    grid[i][j] = '0';
 
-                while (!q.empty()) {
-                    int row = q.front().first;
-                    int col = q.front().second;
-                    q.pop();
+                    while (!q.empty()) {
+                        int row = q.front().first;
+                        int col = q.front().second;
+                        q.pop();
 
-                    int delRow[] = {1, 0, -1, 0};
-                    int delCol[] = {0, 1, 0, -1};
+                        int delRow[] = {1, 0, -1, 0};
+                        int delCol[] = {0, 1, 0, -1};
 
-                    for (int i = 0; i < 4; i++) {
-                        int nrow = row + delRow[i];
-                        int ncol = col + delCol[i];
+                        for (int i = 0; i < 4; i++) {
+                            int nrow = row + delRow[i];
+                            int ncol = col + delCol[i];
 
-                        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                            !visited[nrow][ncol] && grid[nrow][ncol] == '1') {
-                            visited[nrow][ncol] = true;
-                            q.push({nrow, ncol});
+                            if (nrow >= 0 && nrow < n && ncol >= 0 &&
+                                ncol < m && grid[nrow][ncol] == '1') {
+                                grid[nrow][ncol] = 0;
+                                q.push({nrow, ncol});
+                            }
                         }
                     }
+                    ans++;
                 }
-                ans++;
             }
         }
 
