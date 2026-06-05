acd0e5063575ed2a17696dbf8ebda95e0b26d27d
Vishal kumar <vishalkumargupta4546@gmail.com>
2026-06-05 15:29:47 +0530

    Time: 49 ms (45.20%) | Memory: 66.3 MB (35.12%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -3,32 +3,33 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
 
         int n = graph.size();
-        vector<int> indegree(n, 0);
-        vector<vector<int>> revGraph(n);
+        vector<int> outDeg(n);
 
+        vector<vector<int>> adj(n);
         for (int i = 0; i < n; i++) {
-            for (auto it : graph[i]) {
-                revGraph[it].push_back(i);
-                indegree[i]++;
+            for (auto& it : graph[i]) {
+                adj[it].push_back(i);
+                outDeg[i]++;
             }
         }
 
         queue<int> q;
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
+        vector<int> ans;
+        for(int i=0; i<n; i++){
+            if(!outDeg[i]){
                 q.push(i);
             }
         }
 
-        vector<int> ans;
-        while (!q.empty()) {
+        while(!q.empty()){
             int node = q.front();
-            ans.push_back(node);
             q.pop();
 
-            for (auto it : revGraph[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0) {
+            ans.push_back(node);
+
+            for(auto& it : adj[node]){
+                outDeg[it]--;
+                if(outDeg[it]==0){
                     q.push(it);
                 }
             }
