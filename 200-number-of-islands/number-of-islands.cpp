12dd4678482b2b8260bf54606e4fd1ceebd47506
Vishal kumar <vishalkumargupta4546@gmail.com>
2026-06-28 14:15:11 +0530

    Time: 29 ms (41.98%) | Memory: 18.7 MB (37.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-
-        int ans = 0;
         int n = grid.size();
         int m = grid[0].size();
+
+        int island = 0;
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
         queue<pair<int, int>> q;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
-                if (grid[i][j] == '1') {
+                if (grid[i][j] == '1' && !visited[i][j]) {
                     q.push({i, j});
-                    grid[i][j] = '0';
+                    visited[i][j] = true;
 
                     while (!q.empty()) {
                         int row = q.front().first;
                         int col = q.front().second;
                         q.pop();
 
-                        int delRow[] = {1, 0, -1, 0};
-                        int delCol[] = {0, 1, 0, -1};
+                        int delRow[4] = {-1, 0, 1, 0};
+                        int delCol[4] = {0, 1, 0, -1};
 
                         for (int i = 0; i < 4; i++) {
                             int nrow = row + delRow[i];
                             int ncol = col + delCol[i];
 
                             if (nrow >= 0 && nrow < n && ncol >= 0 &&
-                                ncol < m && grid[nrow][ncol] == '1') {
-                                grid[nrow][ncol] = 0;
+                                ncol < m && grid[nrow][ncol] == '1' &&
+                                !visited[nrow][ncol]) {
+                                visited[nrow][ncol] = true;
                                 q.push({nrow, ncol});
                             }
                         }
                     }
-                    ans++;
+                    island++;
                 }
             }
         }
-
-        return ans;
+        return island;
     }
 };
