be08bb57941c696f02535a3a1afaab6a15402ef0
Vishal kumar <vishalkumargupta4546@gmail.com>
2026-08-02 13:15:10 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (45.85%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -6,24 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int pathSum(TreeNode* root, int& maxi){
-        if(root==NULL) return 0;
+    int pathSum(TreeNode* root, int& maxSum) {
+        if (root == NULL)
+            return 0;
 
-        int leftSum = max(0, pathSum(root->left, maxi));
-        int rightSum = max(0, pathSum(root->right, maxi));
+        int leftSum = max(0, pathSum(root->left, maxSum));
+        int rightSum = max(0, pathSum(root->right, maxSum));
 
-        maxi = max(maxi, root->val + leftSum + rightSum);
+        maxSum = max(maxSum, root->val + leftSum + rightSum);
         return root->val + max(leftSum, rightSum);
     }
 
     int maxPathSum(TreeNode* root) {
-        int maxi = INT_MIN;
-        pathSum(root, maxi);
-        return maxi;
+        int maxSum = INT_MIN;
+        pathSum(root, maxSum);
+        return maxSum;
     }
 };
