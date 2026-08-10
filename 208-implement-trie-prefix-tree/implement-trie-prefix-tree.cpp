2bbabcace2e19bb1722fc10fb808963f4b5a2f9e
Vishal kumar <vishalkumargupta4546@gmail.com>
2026-08-10 20:32:36 +0530

    Time: 23 ms (45.03%) | Memory: 50.5 MB (79.09%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -2,49 +2,54 @@ class TrieNode {
 public:
     TrieNode* children[26];
     bool isEndOfWord;
+
     TrieNode() {
         isEndOfWord = false;
-        for (int i = 0; i < 26; i++)
+        for (int i = 0; i < 26; i++) {
             children[i] = nullptr;
+        }
     }
 };
 
 class Trie {
-private:
-    TrieNode* root;
-
 public:
-    Trie() { root = new TrieNode(); }
+    TrieNode* root;
+    Trie() {
+        root = new TrieNode();
+    }
 
     void insert(string word) {
         TrieNode* node = root;
-        for (char c : word) {
-            int index = c - 'a';
-            if (!node->children[index])
-                node->children[index] = new TrieNode();
-            node = node->children[index];
+        for(auto& i : word){
+            int idx = i-'a';
+            if(!node->children[idx]){
+                node->children[idx] = new TrieNode();
+            }
+            node = node->children[idx];
         }
         node->isEndOfWord = true;
     }
 
     bool search(string word) {
         TrieNode* node = root;
-        for (char c : word) {
-            int index = c - 'a';
-            if (!node->children[index])
+        for(auto& i : word){
+            int idx = i-'a';
+            if(!node->children[idx]){
                 return false;
-            node = node->children[index];
+            }
+            node = node->children[idx];
         }
         return node->isEndOfWord;
     }
 
     bool startsWith(string prefix) {
         TrieNode* node = root;
-        for (char c : prefix) {
-            int index = c - 'a';
-            if (!node->children[index])
+        for(auto& i : prefix){
+            int idx = i-'a';
+            if(!node->children[idx]){
                 return false;
-            node = node->children[index];
+            }
+            node = node->children[idx];
         }
         return true;
     }
