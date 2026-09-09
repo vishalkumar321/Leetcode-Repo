cb301d6dfa88e5b9109b6a0eb5a80e9db83b2473
Vishal kumar <vishalkumargupta4546@gmail.com>
2026-09-09 15:57:18 +0530

    Time: 1763 ms (12.68%) | Memory: 9.4 MB (77.41%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,38 +1,39 @@
+// TOP-DOWN APPROACH
 class Solution {
 public:
-    bool isPalin(int i, int j, string& s) {
+    bool isPalindrome(int i, int j, string& s) {
         while (i < j) {
-            if (s[i] != s[j]) {
+            if (s[i] != s[j])
                 return false;
-            }
             i++;
             j--;
         }
         return true;
     }
 
-    int helper(int i, int n, string& s, vector<int>& dp) {
-        if (i == n) {
+    int solve(int i, int n, string& s, vector<int>& dp) {
+        if (i == n)
             return 0;
-        }
         if (dp[i] != -1) {
             return dp[i];
         }
 
-        int palin = INT_MAX;
+        int mini = INT_MAX;
         for (int j = i; j < n; j++) {
-            if (isPalin(i, j, s)) {
-                int cost = 1 + helper(j + 1, n, s, dp);
-                palin = min(palin, cost);
+            if (isPalindrome(i, j, s)) {
+                int cost = 1 + solve(j + 1, n, s, dp);
+                mini = min(mini, cost);
             }
         }
-        dp[i] = palin;
+
+        dp[i] = mini;
         return dp[i];
     }
 
     int minCut(string s) {
         int n = s.size();
+
         vector<int> dp(n, -1);
-        return helper(0, n, s, dp) - 1;
+        return solve(0, n, s, dp) - 1;
     }
 };
